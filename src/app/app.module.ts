fa68dba4b0e76dd4b338038a237560f6fc0df5dc
Osvaldo Calombe <osvaldo.calombe@ucall.co.ao>
2021-06-24 21:29:22 +0100

    estruturado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { BlankComponent } from './components/main/blank/blank.component';
+import { LoginComponent } from './components/main/login/login.component';
+import { DashboardComponent } from './components/main/dashboard/dashboard.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    BlankComponent,
+    LoginComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
